7dffa7f953d40687cf5bbb50eabfa9fdf3dd5c0e
Patogomezfa <patogomezfa@gmail.com>
2021-06-24 22:10:51 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  //state de la app
+  const [busqueda, guardarBusqueda] = useState('');
+
+    useEffect(() => {
+      const consultarAPI = async () => {
+        if(busqueda === '') return;
+
+        const imagenesPorPagina = 30;
+        const key = '462861-ace02d1f4af082d14ea435818';
+        const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+  
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        guardarBusqueda(resultado.hits);
+      }
+      consultarAPI();
+
+    }, [busqueda])
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
-        
-        <Formulario />
+
+        <Formulario 
+          guardarBusqueda={guardarBusqueda}
+        />
       </div>
     </div>
   );
